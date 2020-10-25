89264019df67fe82bc0e45b7bc2f61f258bdf327
JaumeMontagut <jaume.montagut.i.guix@gmail.com>
2020-10-25 16:39:09 +0100

    getting started with windows socket library from the documentation

--- Sockets/Sockets/UDP Server/main.cpp
+++ Sockets/Sockets/UDP Server/main.cpp
@@ -1,8 +1,20 @@
+//Server
 #include <stdlib.h>
 #include <stdio.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
 
-int main() {
+int main(int argc, char** argv) {
 	printf("Hello, this is the UDP server!\n");
+
+	WSADATA wsaData;
+	int iResult;
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		printf("WSAStartup failed: %d\n", iResult);
+		return 1;
+	}
+
 	system("pause");
 	return 0;
 }
