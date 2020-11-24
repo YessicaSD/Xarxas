10af9cf228ce53520ac2d308a306d5e2630b499f
JaumeMontagut <jaume.montagut.i.guix@gmail.com>
2020-11-24 22:28:12 +0100

    removing own replication command

--- Project5/Multiplayer Game/ReplicationCommand.h
+++ Project5/Multiplayer Game/ReplicationCommand.h
@@ -1,18 +1,3 @@
 #pragma once
 
 // TODO(you): World state replication lab session
-
-union ReplicationCommand {
-	//ReplicationActionType actionType;
-
-	struct destroyCommand {
-		int networkId;
-		//nothings
-	};
-
-	struct creationCommand{
-		int networkId;
-		//position
-		//rotation
-	};
-};
