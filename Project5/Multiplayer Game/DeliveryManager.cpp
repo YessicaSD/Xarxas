d115ea4317cda3a75ad9f85909e35fa65e9f8035
JaumeMontagut <jaume.montagut.i.guix@gmail.com>
2020-12-24 12:58:21 +0100

    pending acknowledgments should work differently

--- Project5/Multiplayer Game/DeliveryManager.cpp
+++ Project5/Multiplayer Game/DeliveryManager.cpp
@@ -19,24 +19,16 @@ bool DeliveryManagerClient::processSequenceNumber(const InputMemoryStream& packe
 {
     uint32 sequenceNumber = 0;
     packet >> sequenceNumber;
-    if (sequenceNumber == expectedSequenceNum) {
+    
+    pendingAck.push_back(sequenceNumber);
+
+    if (sequenceNumber >= expectedSequenceNum) {
         //send success
         expectedSequenceNum = expectedSequenceNum + 1;
     }
     else if (sequenceNumber < expectedSequenceNum) {
-        //choose to dicard the packet or whatever
-        //remove from pending ack
         //send success
     }
-    else if (sequenceNumber > expectedSequenceNum){
-        for (int i = expectedSequenceNum; i < sequenceNumber; ++i) {
-            if (std::find(pendingAck.front(), pendingAck.back(), i)) {
-                pendingAck.push_back(i);
-            }
-        }
-        //TODO : send packet to server to resend the pending acknowledgements
-        //send success
-        expectedSequenceNum = expectedSequenceNum + 1;
-    }
+
     return false;
 }
